f409dfc4d41f43373b1a4b411ecb96099f41b167
Philippe Barthelemy <philb75@gmail.com>
2025-01-10 00:31:47 +0100

    Update GitHub Actions workflow and refactor main entry point to include App component and improve rendering structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,19 @@
+import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App'
 import ChatWidget from './components/ChatWidget'
 import './index.css'
 
+// Mount main app
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
+
+// Mount chat widget
 ReactDOM.createRoot(document.getElementById('chat-widget')!).render(
-  <>
+  <React.StrictMode>
     <ChatWidget />
-  </>
+  </React.StrictMode>
 )
